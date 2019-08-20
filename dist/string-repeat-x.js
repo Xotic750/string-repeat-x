864f50e0972179ee4c3b193a7fe9c7a1f5017f53
Graham Fairweather <xotic750@gmail.com>
2019-08-20 20:30:56 +0200

    Maintenance

--- dist/string-repeat-x.js
+++ dist/string-repeat-x.js
@@ -2,7 +2,7 @@
 {
   "author": "Graham Fairweather",
   "copywrite": "Copyright (c) 2019-present",
-  "date": "2019-08-19T12:57:06.545Z",
+  "date": "2019-08-20T18:30:48.310Z",
   "describe": "",
   "description": "Constructs and returns a new string which contains the specified number of copies of the string.",
   "file": "string-repeat-x.js",
